af1107cae92219569f4846cf07581c690658c207
Ashley Evans <ashley_e@live.com>
2019-03-23 12:22:19 -0700

    Adding images and updated information to GitHub

---
+++ assets/javascript/app.js
@@ -0,0 +1,61 @@
+var userChoices = document.getElementsByTagName('input[type:radio]');
+var questions = [ {
+          question: "What is the name of Will Smith’s character in Independence Day?",
+          choices: ["Captain Steven Hiller", "Sargent Dan", "Michael Jordan", "Ryan Reynolds"],
+          answer: 1
+        },
+        
+        {
+          question: "Which 1997 film stars Nicolas Cage, John Cusack, and John Malkovich?",
+          choices: ["Face Off", "Con Air", "Gone in 60 seconds", "National Treasure"],
+          answer: 2
+        },
+
+        {
+            question: "How many people were killed in the 1996 film Scream?",
+            choices: ["8", "5", "7", "4"],
+            answer: 3
+        },
+          
+        {
+            question: "What year was Forrest Gump released?",
+            choices: ["1987", "1992", "1998", "1994"],
+            answer: 4
+        },
+
+        {
+            question: "What year was the song “My Heart Will Go On” from Titanic released?",
+            choices: ["1985", "1992", "1997", "1995"],
+            answer: 1
+        },
+          
+        {
+            question: "Which 90’s movie soundtrack is the best-selling soundtrack of all time?",
+            choices: ["Titanic", "The Bodyguard", "Forrest Gump", "Fast and the Furious"],
+            answer: 2
+        },
+  
+        {
+              question: "What movie is this quote from? You can’t handle the truth!",
+              choices: ["Home Alone", "Rush Hour", "The Sixth Sense", "A Few Good Men"],
+              answer: 4
+        },
+            
+        {
+              question: "Which 90’s animated film was Disney’s first ever DVD?",
+              choices: ["Fern Gully", "Mulan", "Lion King", "Hercules"],
+              answer: 2
+        },
+
+        {
+            question: "In Jumanji, a stampede is released. What is the slowest animal??",
+            choices: ["Zebra", "Antelope", "Rhinoseros", "Lion"],
+            answer: 3
+      },
+          
+      {
+            question: "What was Adam Sandler's occupation in Big Daddy?",
+            choices: ["Janitor", "Construction Worker", "Landscaper", "Toll Booth Worker"],
+            answer: 4
+      }];
+
